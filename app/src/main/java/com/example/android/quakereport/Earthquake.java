a469638d669c807477642d10aecb1c8791e212db
Anubhav7298 <61357115+Anubhav7298@users.noreply.github.com>
2020-11-03 14:31:31 +0530

    updated removed bugs from previous commit

--- app/src/main/java/com/example/android/quakereport/Earthquake.java
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -16,16 +16,36 @@ public class Earthquake {
 
 
     public String getCity() {
+        String S="";
+        if(mCity.contains("of")){
+        String[] arr = mCity.split("of",2);
+        arr[0] = arr[0] + "of";
+        S = arr[0];}
 
-        String []arr = mCity.split("of ",2);
-        arr[0] = arr[0] + " of ";
-        return arr[0];
+        if(mCity.contains("the")){
+        String[] arr = mCity.split("the",2);
+        arr[0] = arr[0] + "the";
+            S = arr[0];}
+        return S;
     }
 
     public String getSplitCity() {
+          String S="";
+        if(mCity.contains("of")){
+        for(int j = 0 ; j < mCity.length() ; j++) {
+            if (mCity.charAt(j) == 'o' && mCity.charAt(j + 1) == 'f'){
+                S=mCity.substring((j + 2), (mCity.length()));
+            break;}
+        }}
+        if(mCity.contains("the")){
+            for(int j = 0 ; j < mCity.length() ; j++) {
+                if (mCity.charAt(j) == 't' && mCity.charAt(j + 1) == 'h' && mCity.charAt(j+2)== 'e'){
+                    S=mCity.substring((j + 3), (mCity.length()));
+                    break;}
+            }}
 
-        String []arr = mCity.split("of ",2);
-        return arr[1];
+        return S;
+
     }
 
     public long getTime() {
